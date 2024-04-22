3e87dd9e71ce684b3a058cc0142869ba92085574
Glitch (glitch-hello-sqlite) <none>
2024-04-22 23:26:27 +0000

    🐕🌗 Checkpoint ./index.html:45080849/737 ./assets/css/styles.css:45080849/289 ./assets/js/app.js:45080849/122

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,4 @@
 console.log("%c\uf8ff Reminders Clone", "font-family: 'SF Pro Display', sans-serif; font-size: large; background-color: black; color: white; border-radius: 0.5rem; padding: 5px;");
+
+var reminders = reminders || {};
+
